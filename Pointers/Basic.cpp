ad5c1010f869735a601379baf7b2677b1e808277
Mohit Yadav <my266620@gmail.com>
2025-09-01 23:16:25 +0530

    pointer2

--- Pointers/Basic.cpp
+++ Pointers/Basic.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 int main()
 {
-    int a = 5;
+    // int a = 5;
     // int b = 5;
 
     // cout << a << endl;
@@ -12,11 +12,11 @@ int main()
 
     // Pointer=> in c++ is a variable that store address of another variable
 
-    int *ptr = &a;
-    cout << "Address of a is: " << &a << endl;           // print address
-    cout << "Address stored in ptr is: " << ptr << endl; // print address but it is address &a
-    cout << "Value stored at address ptr points to is: " << *ptr << endl;
-    cout << "Address of ptr is: " << &ptr << endl;
+    // int *ptr = &a;
+    // cout << "Address of a is: " << &a << endl;           // print address
+    // cout << "Address stored in ptr is: " << ptr << endl; // print address but it is address &a
+    // cout << "Value stored at address ptr points to is: " << *ptr << endl;
+    // cout << "Address of ptr is: " << &ptr << endl;
 
     // Sizeof of ptr is 8 either it is int,char or double type
 
@@ -24,5 +24,23 @@ int main()
 
     // Null Pointer
     int *ptr = 0;
-    cout << *ptr << endl;
+    // cout << *ptr << endl;
+
+    // Exampleeee
+    int a = 15;
+    int *p = &a;
+    int *q = p;
+    int *r = q;
+
+    cout << a << endl;  // 15
+    cout << &a << endl; // address of a
+    cout << p << endl;  // address of a
+    cout << &p << endl; // address of p
+    cout << *p << endl; // 15
+    cout << q << endl;  // address of a
+    cout << &q << endl; // address of q
+    cout << *q << endl; // 15
+    cout << r << endl;  // address of a
+    cout << &r << endl; // address of r
+    cout << *r << endl; // 15
 }
---
+++ Pointers/Pointer_2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[4] = {20, 1, 2, 14};
+
+    // cout << arr << endl; // Base Address
+    // // But agr hum int *p =&arr    ,cout<<p =>value(address of arr) inside p
+
+    // cout << &arr << endl; // Base Address
+    // // But agr hum int *p =&arr   , cout<<& => give address of p
+
+    // cout << &arr[0] << endl; // Base Address
+
+    // cout << arr[0] << endl; // Value at 0th index
+
+    // Dono Baat same hai
+    // cout << arr[0] << endl;
+    // cout << *arr << endl;
+    // cout << *arr + 1 << endl;
+    // cout << *(arr + 1) << endl;
+
+    // Intresting thing try
+    int i = 0;
+    cout << arr[i] << endl; // 20  & in all below case
+    cout << i[arr] << endl; // New
+    cout << *(arr + i) << endl;
+    cout << *(i + arr) << endl;
+}
