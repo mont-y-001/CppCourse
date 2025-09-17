e9b948064d50f36f42b1cc4150ad01c43bfd8708
Mohit Yadav <my266620@gmail.com>
2025-09-17 22:55:35 +0530

    .

---
+++ Sorting/InsertionSort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void InsertionSort(vector<int> &arr, int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int temp = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = temp;
+    }
+}
+
+int main()
+{
+    vector<int> arr = {9, 3, 13, 1, 4, 0};
+    int n = arr.size();
+
+    InsertionSort(arr, n);
+
+    for (int x : arr)
+    {
+        cout << x << " ";
+    }
+    return 0;
+}
