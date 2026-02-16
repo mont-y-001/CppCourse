c0cdea77159736eaf422519de2590ca2cf0d4d3b
Mohit Yadav <my266620@gmail.com>
2026-02-16 23:08:58 +0530

    /

---
+++ Array/Target_sum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[7] = {2, 1, 7, 8, 5, 4, 3};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target = 11;
+    for (int i = 0; i < size; i++)
+    {
+    }
+}
--- Sliding Window/Basic.cpp
+++ Sliding Window/Basic.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
 int maxSum(vector<int> arr, int k, int n)
 {
-    int MSum = 0;
+    int windowSum = 0;
+
     for (int i = 0; i < k; i++)
+        windowSum += arr[i];
+
+    int maxi = windowSum;
+
+    for (int i = k; i < n; i++)
     {
-        MSum += arr[i];
+        windowSum = windowSum - arr[i - k] + arr[i];
+        maxi = max(maxi, windowSum);
     }
-    for (int i = 0; i < n; i++)
-    {
-        if (i + 3 > n)
-        {
-            break;
-        }
-        int currSum = MSum - arr[i] + arr[i + 3];
-        int maxi = max(currSum, MSum);
-    }
+
     return maxi;
 }
+
 int main()
 {
     vector<int> arr = {2, 3, 1, 8, 7, 5, 4};
-    int n = arr.size();
     int k = 3;
-    maxSum(arr, k, n);
+
+    cout << maxSum(arr, k, arr.size());
 }
